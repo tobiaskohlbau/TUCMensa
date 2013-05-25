eeb8642ee9c196166b8fe3b926c86fb252d1ba86
Tobias Kohlbau <tobias.kohlbau@s2011.tu-chemnitz.de>
2013-05-25 23:31:41 +0200

    added TUCMensa activity and set as main activity
    Signed-off-by: Tobias Kohlbau <tobias.kohlbau@s2011.tu-chemnitz.de>

---
+++ TUCMensa/src/main/java/de/kohlbau/tucmensa/TUCMensa.java
@@ -0,0 +1,13 @@
+package de.kohlbau.tucmensa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tobias on 5/25/13.
+ */
+public class TUCMensa extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
